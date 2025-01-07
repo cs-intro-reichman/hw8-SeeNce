b9a6edae06a8820bfe21a1273f3c07ca38dfa0d4
Mor Cohen <mor.cohen02@post.runi.ac.il>
2025-01-07 17:22:54 +0200

    network2

--- Network.java
+++ Network.java
@@ -53,7 +53,7 @@ public class Network {
      *  If any of the two names is not a user in this network,
      *  or if the "follows" addition failed for some reason, returns false. */
     public boolean addFollowee(String name1, String name2) {
-        if(getUser(name1) == null || getUser(name2) == null || name1.isEmpty() || name2.isEmpty() || name1 == null || name2 == null) return false;
+        if(getUser(name1) == null || getUser(name2) == null || name1.isEmpty() || name2.isEmpty() || name1 == null || name2 == null || name1.equals(name2)) return false;
 
         return getUser(name1).addFollowee(name2);
     }
@@ -83,6 +83,8 @@ public class Network {
     /** Computes and returns the name of the most popular user in this network: 
      *  The user who appears the most in the follow lists of all the users. */
     public String mostPopularUser() {
+        if(this.userCount == 0) return null;
+
         int userPopularityArr[] = new int[userCount];
         int popularUserIndex = -1;
         int max = -1;
